51b88aebacacbd0e641f72e504ffef3ab3997ff0
CodyKoInABox <codykoinabox@gmail.com>
2023-12-03 19:24:40 -0300

    fix database name typo

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 8080
 
 const mysql = require('mysql2')
 
-// connect to a mysql database named produtosN3
+// connect to a mysql database named produtoN3
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'produtosN3' // aqui vai o nome da database, ela deve seguir o padrao encontrado no README.md deste repositorio
+    database: 'produtoN3' // aqui vai o nome da database, ela deve seguir o padrao encontrado no README.md deste repositorio
 })
 
 app.get('/ping', (req, res) => {
